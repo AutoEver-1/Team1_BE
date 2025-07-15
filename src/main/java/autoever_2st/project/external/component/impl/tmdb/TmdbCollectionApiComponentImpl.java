4e3eeb09485860908bfe7cd30e7ae72175314376
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-15 12:43:15 +0900

    #2 feat : Tmdb API 관련 하위 구현 클래스 선언 - Tmdb Collection 관련 api call

---
+++ src/main/java/autoever_2st/project/external/component/impl/tmdb/TmdbCollectionApiComponentImpl.java
@@ -0,0 +1,47 @@
+package autoever_2st.project.external.component.impl.tmdb;
+
+import autoever_2st.project.exception.exception_class.business.BusinessException;
+import autoever_2st.project.external.dto.tmdb.common.collection.CollectionImagesWrapperDto;
+import autoever_2st.project.external.dto.tmdb.common.collection.CollectionWrapperDto;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@Qualifier("tmdbCollection")
+@Getter
+public class TmdbCollectionApiComponentImpl extends TmdbApiComponentImpl{
+    private final RestClient restClient;
+
+    public TmdbCollectionApiComponentImpl() {
+        this.restClient = getCollectionRestClient();
+    }
+
+    public CollectionWrapperDto getCollection(Integer collectionId) {
+        try {
+            return restClient.get()
+                    .uri(uriBuilder -> uriBuilder.path("/{collectionId}/ko-KR")
+                            .build(collectionId))
+                    .retrieve()
+                    .body(CollectionWrapperDto.class);
+        } catch (Exception e) {
+            throw new BusinessException("getCollection 에러 발생", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public CollectionImagesWrapperDto getCollectionImages(Integer collectionId) {
+        try {
+            return restClient.get()
+                    .uri(uriBuilder -> uriBuilder.path("/{collectionId}/images")
+                            .build(collectionId))
+                    .retrieve()
+                    .body(CollectionImagesWrapperDto.class);
+        } catch (Exception e) {
+            throw new BusinessException("getCollectionImages 에러 발생", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+}
