eb1b6f589176a177b635d6e1453b89d984e4fa28
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-15 12:37:40 +0900

    #2 feat : Genre 관련 Dto Feature

---
+++ src/main/java/autoever_2st/project/external/dto/tmdb/response/movie/GenreDto.java
@@ -0,0 +1,15 @@
+package autoever_2st.project.external.dto.tmdb.response.movie;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class GenreDto {
+    @JsonProperty("id")
+    private Integer id;
+    
+    @JsonProperty("name")
+    private String name;
+}
