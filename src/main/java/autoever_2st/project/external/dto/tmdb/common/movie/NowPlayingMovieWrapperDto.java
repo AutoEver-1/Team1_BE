75be35e9e7eb86bea3e024b05e647c5fc642aa6d
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-15 12:39:35 +0900

    #2 feat : Movie 관련 Dto Feature

---
+++ src/main/java/autoever_2st/project/external/dto/tmdb/common/movie/NowPlayingMovieWrapperDto.java
@@ -0,0 +1,28 @@
+package autoever_2st.project.external.dto.tmdb.common.movie;
+
+import autoever_2st.project.external.dto.tmdb.response.movie.DatesDto;
+import autoever_2st.project.external.dto.tmdb.response.movie.MovieResponseDto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+public class NowPlayingMovieWrapperDto {
+    @JsonProperty("dates")
+    private DatesDto dates;
+
+    @JsonProperty("page")
+    private Integer page;
+
+    @JsonProperty("results")
+    private List<MovieResponseDto> results;
+
+    @JsonProperty("total_pages")
+    private Integer totalPages;
+
+    @JsonProperty("total_results")
+    private Integer totalResults;
+}
