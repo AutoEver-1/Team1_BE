5d7ef98f5d1f673d14fa3ae9bc9787cf5934216b
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-15 12:39:17 +0900

    #2 feat : Tv Series 관련 Dto Feature

---
+++ src/main/java/autoever_2st/project/external/dto/tmdb/response/trend/NextEpisodeToAirDto.java
@@ -0,0 +1,9 @@
+package autoever_2st.project.external.dto.tmdb.response.trend;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class NextEpisodeToAirDto extends LastEpisodeToAirDto{
+}
