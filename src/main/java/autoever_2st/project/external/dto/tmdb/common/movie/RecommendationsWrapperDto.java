3594abd3290783c6548c5228a62dd17668ac9d99
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-15 12:41:13 +0900

    #2 feat : Recommendation Movie 관련 Dto Feature

---
+++ src/main/java/autoever_2st/project/external/dto/tmdb/common/movie/RecommendationsWrapperDto.java
@@ -0,0 +1,10 @@
+package autoever_2st.project.external.dto.tmdb.common.movie;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class RecommendationsWrapperDto extends PopularMovieWrapperDto {
+
+}
