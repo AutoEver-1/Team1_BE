f0228d38f4020773566111dfcb72b728a7da9fd6
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-15 12:41:39 +0900

    #2 feat : Tmdb Season 관련 Dto Feature

---
+++ src/main/java/autoever_2st/project/external/dto/tmdb/response/trend/SeasonDto.java
@@ -0,0 +1,26 @@
+package autoever_2st.project.external.dto.tmdb.response.trend;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class SeasonDto {
+    @JsonProperty("air_date")
+    private String airDate;
+    @JsonProperty("episode_count")
+    private Integer episodeCount;
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("overview")
+    private String overview;
+    @JsonProperty("poster_path")
+    private String posterPath;
+    @JsonProperty("season_number")
+    private Integer seasonNumber;
+    @JsonProperty("vote_average")
+    private Double voteAverage;
+}
