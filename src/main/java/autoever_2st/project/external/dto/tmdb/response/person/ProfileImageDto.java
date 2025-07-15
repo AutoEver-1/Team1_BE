28aca408552ff09f386449a9833e4fe624669b27
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-15 12:40:53 +0900

    #2 feat : Profile Image 관련 Dto Feature

---
+++ src/main/java/autoever_2st/project/external/dto/tmdb/response/person/ProfileImageDto.java
@@ -0,0 +1,30 @@
+package autoever_2st.project.external.dto.tmdb.response.person;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ProfileImageDto {
+    @JsonProperty("aspect_ratio")
+    private Double aspectRatio;
+    
+    @JsonProperty("height")
+    private Integer height;
+    
+    @JsonProperty("iso_639_1")
+    private String iso6391;
+    
+    @JsonProperty("file_path")
+    private String filePath;
+    
+    @JsonProperty("vote_average")
+    private Double voteAverage;
+    
+    @JsonProperty("vote_count")
+    private Integer voteCount;
+    
+    @JsonProperty("width")
+    private Integer width;
+}
