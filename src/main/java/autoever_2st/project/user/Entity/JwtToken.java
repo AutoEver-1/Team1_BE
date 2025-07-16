ae8446ac3197645d56fd1e70b7fdee5d75e7d9d8
Yoo-Hyuna <yha201341@gmail.com>
2025-07-16 14:23:00 +0900

    refactor(Jwt_token): jwt token 테이블에 timestamp 필드 추가
    - Jwt_token 테이블에 create, delete, update에 대한 timestamp를 저장하는 필드를 추가했습니다.

--- src/main/java/autoever_2st/project/user/Entity/JwtToken.java
+++ src/main/java/autoever_2st/project/user/Entity/JwtToken.java
@@ -2,6 +2,11 @@ package autoever_2st.project.user.Entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
@@ -9,6 +14,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class JwtToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,5 +29,14 @@ public class JwtToken {
 
     @Column(length = 1000)
     private String refreshToken;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt; // soft delete용 수동 처리
 
 }
