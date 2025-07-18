49bf91475ced3dc231fd275f9a1b12dff4adee0b
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-18 15:01:57 +0900

    #2 feat : Jdbc template에서 사용되는 sql query를 중앙집중화하기위한 클래스 feature

---
+++ src/main/java/autoever_2st/project/jdbc/constants/SqlConstants.java
@@ -0,0 +1,90 @@
+package autoever_2st.project.jdbc.constants;
+
+/**
+ * SQL 쿼리 상수 클래스
+ * 
+ * 애플리케이션에서 사용되는 모든 SQL 쿼리를 중앙 집중화하여 관리.
+ */
+public class SqlConstants {
+
+    // TmdbMovieDetail 관련 쿼리
+    public static final String FIND_EXISTING_MOVIE_DETAILS = 
+            "SELECT id, tmdb_id, title, updated_at FROM tmdb_movie_detail " +
+            "WHERE tmdb_id IN (:tmdbIds)";
+
+    public static final String INSERT_MOVIE_DETAIL = 
+            "INSERT INTO tmdb_movie_detail (is_adult, tmdb_id, title, original_title, " +
+            "original_language, overview, status, release_date, runtime, video, " +
+            "vote_average, vote_count, popularity, media_type, registed_at, updated_at) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    public static final String UPDATE_MOVIE_DETAIL = 
+            "UPDATE tmdb_movie_detail SET title = ?, original_title = ?, overview = ?, " +
+            "vote_average = ?, vote_count = ?, popularity = ?, updated_at = ? " +
+            "WHERE id = ?";
+
+    // MovieGenre 관련 쿼리
+    public static final String FIND_EXISTING_GENRES = 
+            "SELECT id, genre_id, name FROM movie_genre " +
+            "WHERE genre_id IN (:genreIds)";
+
+    public static final String FIND_ALL_GENRES = 
+            "SELECT id, genre_id, name FROM movie_genre";
+
+    public static final String INSERT_GENRE = 
+            "INSERT INTO movie_genre (genre_id, name, registed_at, updated_at) " +
+            "VALUES (?, ?, ?, ?)";
+
+    public static final String UPDATE_GENRE = 
+            "UPDATE movie_genre SET name = ?, updated_at = ? " +
+            "WHERE id = ?";
+
+    // MovieGenreMatch 관련 쿼리
+    public static final String INSERT_GENRE_MATCH = 
+            "INSERT INTO movie_genre_match (tmdb_movie_detail_id, movie_genre_id, registed_at, updated_at) " +
+            "VALUES (?, ?, ?, ?)";
+
+    // Movie 관련 쿼리
+    public static final String FIND_EXISTING_MOVIES_BY_TMDB_ID = 
+            "SELECT id, tmdb_movie_detail_id FROM movie " +
+            "WHERE tmdb_movie_detail_id IN (:tmdbMovieDetailIds)";
+
+    public static final String INSERT_MOVIE = 
+            "INSERT INTO movie (tmdb_movie_detail_id, registed_at, updated_at) " +
+            "VALUES (?, ?, ?)";
+
+    public static final String UPDATE_MOVIE = 
+            "UPDATE movie SET updated_at = ? " +
+            "WHERE id = ?";
+
+    // OttPlatform 관련 쿼리
+    public static final String FIND_EXISTING_OTT_PLATFORMS = 
+            "SELECT id, tmdb_ott_id, name FROM ott_platform " +
+            "WHERE tmdb_ott_id IN (:tmdbOttIds)";
+
+    public static final String FIND_ALL_OTT_PLATFORMS = 
+            "SELECT id, tmdb_ott_id, name FROM ott_platform";
+
+    public static final String INSERT_OTT_PLATFORM = 
+            "INSERT INTO ott_platform (tmdb_ott_id, name, registed_at, updated_at) " +
+            "VALUES (?, ?, ?, ?)";
+
+    public static final String UPDATE_OTT_PLATFORM = 
+            "UPDATE ott_platform SET name = ?, updated_at = ? " +
+            "WHERE id = ?";
+
+    // TmdbMovieDetailOtt 관련 쿼리
+    public static final String INSERT_MOVIE_DETAIL_OTT = 
+            "INSERT INTO tmdb_movie_detail_ott (tmdb_movie_detail_id, ott_platform_id, registed_at, updated_at) " +
+            "VALUES (?, ?, ?, ?)";
+
+    // TmdbMovieImages 관련 쿼리
+    public static final String INSERT_MOVIE_IMAGES = 
+            "INSERT INTO tmdb_movie_images (image_url, base_url, width, height, ratio, image_type, iso_639_1, tmdb_movie_detail_id, registed_at, updated_at) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    // TmdbMovieVideo 관련 쿼리
+    public static final String INSERT_MOVIE_VIDEO = 
+            "INSERT INTO tmdb_movie_video (video_url, base_url, name, site, video_type, iso_639_1, tmdb_movie_detail_id, registed_at, updated_at) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+}
