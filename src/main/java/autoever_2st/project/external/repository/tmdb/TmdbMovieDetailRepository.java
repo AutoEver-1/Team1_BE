52816858d2bbc6640d643fc0d63da5dbd539c473
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-19 22:56:38 +0900

    #2 feat : TmdbMovideDetail 데이터 한번에 불러오는 findAllByTmdbIdIn feature

--- src/main/java/autoever_2st/project/external/repository/tmdb/TmdbMovieDetailRepository.java
+++ src/main/java/autoever_2st/project/external/repository/tmdb/TmdbMovieDetailRepository.java
@@ -2,9 +2,17 @@ package autoever_2st.project.external.repository.tmdb;
 
 import autoever_2st.project.external.entity.tmdb.TmdbMovieDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+import java.util.List;
+
+@Repository
 public interface TmdbMovieDetailRepository extends JpaRepository<TmdbMovieDetail, Long> {
     Optional<TmdbMovieDetail> findById(Long id);
+
+    @Query("SELECT m FROM TmdbMovieDetail m WHERE m.tmdbId IN :tmdbIds")
+    List<TmdbMovieDetail> findAllByTmdbIdIn(List<Long> tmdbIds);
 }
