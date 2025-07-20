839016f71d441a33aaf9fcac98b1f029aaef1848
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-20 20:01:31 +0900

    #30 feat : BoxOfficeMovieDto teaserVideo 필드 추가

--- src/main/java/autoever_2st/project/movie/dto/BoxOfficeMovieDto.java
+++ src/main/java/autoever_2st/project/movie/dto/BoxOfficeMovieDto.java
@@ -17,9 +17,10 @@ public class BoxOfficeMovieDto {
     private Long cumulativeAttendance;
     private List<DirectorDto> director;
     private String posterPath;
+    private String teaserVideo;
 
     public BoxOfficeMovieDto(Integer rank, Long movieId, List<String> genre, String title,
-                             Date release_date, Long cumulativeAttendance, List<DirectorDto> director, String posterPath) {
+                             Date release_date, Long cumulativeAttendance, List<DirectorDto> director, String posterPath, String teaserVideo) {
         this.rank = rank;
         this.movieId = movieId;
         this.genre = genre;
@@ -28,5 +29,6 @@ public class BoxOfficeMovieDto {
         this.cumulativeAttendance = cumulativeAttendance;
         this.director = director;
         this.posterPath = posterPath;
+        this.teaserVideo = teaserVideo;
     }
 }
