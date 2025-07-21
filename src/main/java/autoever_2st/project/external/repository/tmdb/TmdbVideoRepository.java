65d18d3ad7f80f2bc097918e1b1815e7ff03a2c8
Hojip-Kim <rlaghwlq12@naver.com>
2025-07-21 17:38:39 +0900

    #30 feat : TmdbVideoRepository feature

--- src/main/java/autoever_2st/project/external/repository/tmdb/TmdbVideoRepository.java
+++ src/main/java/autoever_2st/project/external/repository/tmdb/TmdbVideoRepository.java
@@ -7,13 +7,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TmdbVideoRepository extends JpaRepository<TmdbMovieVideo, Long> {
-
     @Query("SELECT v FROM TmdbMovieVideo v WHERE v.tmdbMovieDetail.id IN :ids AND v.videoType = :videoType")
     List<TmdbMovieVideo> findAllByTmdbMovieDetailIdsAndVideoType(@Param("ids") List<Long> ids, @Param("videoType") String videoType);
 
     List<TmdbMovieVideo> findByTmdbMovieDetail_TmdbId(Long tmdbId);
 
+    Optional<TmdbMovieVideo> findFirstByTmdbMovieDetail_IdAndIso6391AndVideoTypeOrderByIdAsc(
+            Long tmdbMovieDetailId, String iso6391, String videoType);
 }
